7f1afaab43ad3f67b119bee1eaa1e4f341f88d51
EmiliaGidlof <emilia.gidlof@outlook.com>
2023-02-15 14:19:14 +0100

    Testar flytta ur bilder ur mapp

--- Dice.js
+++ Dice.js
@@ -7,32 +7,32 @@
 
         if (resultat === 1) {
             console.log("siffran är ett")
-            document.getElementById("bild").innerHTML="<img src='/images/1.png'>";
+            document.getElementById("bild").innerHTML="<img src='1.png'>";
         }
 
         else if (resultat === 2) {
             console.log("siffran är två")
-            document.getElementById("bild").innerHTML="<img src='/images/2.png'>";
+            document.getElementById("bild").innerHTML="<img src='2.png'>";
         }
 
         else if (resultat === 3) {
             console.log("siffran är tre")
-            document.getElementById("bild").innerHTML="<img src='/images/3.png'>";
+            document.getElementById("bild").innerHTML="<img src='3.png'>";
         }
 
         else if (resultat === 4) {
             console.log("siffran är fyra")
-            document.getElementById("bild").innerHTML="<img src='/images/4.png'>";
+            document.getElementById("bild").innerHTML="<img src='4.png'>";
         }
 
         else if (resultat === 5) {
             console.log("siffran är fem")
-            document.getElementById("bild").innerHTML="<img src='/images/5.png'>";
+            document.getElementById("bild").innerHTML="<img src='5.png'>";
         }
 
         else if (resultat === 6) {
             console.log("siffran är sex")
-            document.getElementById("bild").innerHTML="<img src='/images/6.png'>";
+            document.getElementById("bild").innerHTML="<img src='6.png'>";
         }
 
         else {
